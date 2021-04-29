064fa3e1ad245beb4d2871db4e08578a2b7e906b
@maverick0701 <madhavanandcp070@gmail.com>
2021-04-30 05:06:43 +0530

    added toggle

--- src/components/DashBoardToggle.js
+++ src/components/DashBoardToggle.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 export default class DashBoardToggle extends Component {
+  toggleTabs = (e) => {
+    console.log(e.target);
+
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+  };
   render() {
     return (
       <div>
         <div className="tabs">
-          <div className="tab right" id="">
+          <div className="tab active" onClick={this.toggleTabs}>
             Tweets
           </div>
-          <div className="tab active">Hospitals</div>
-          <div className="tab left">Oxygen</div>
-          <div className="tab left">Medicine</div>
+          <div className="tab" onClick={this.toggleTabs}>
+            Hospitals
+          </div>
+          <div className="tab" onClick={this.toggleTabs}>
+            Oxygen
+          </div>
+          <div className="tab" onClick={this.toggleTabs}>
+            Medicine
+          </div>
         </div>
       </div>
     );
